894167bbb166f848d6941aa505871ff31eb4ac8e
TsubasaKondo <Tsubasa@FujiProvingGrounds.com>
2015-10-31 14:45:34 +0900

    Added 4 new movies. Commented out play_trailer, as this was merely a test argument

--- media_center.py
+++ media_center.py
@@ -1,10 +1,29 @@
 import media
 
 goodwill_hunting = media.Movie("Goodwill Hunting",
-                                "Robert Williams and Matt Damon Shenanigans!!!",
-                                "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg",
-                                "https://www.youtube.com/watch?v=z02M3NRtkAA")
+                    "Robert Williams and Matt Damon Shenanigans!!!",
+                    "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg",
+                    "https://www.youtube.com/watch?v=z02M3NRtkAA")
 
-print(goodwill_hunting.storyline_text)
+500_days_of_summer = media.Movie("500 Days of Summer",
+                    "You should know upfront that this is not a love story ;) ",
+                    "https://upload.wikimedia.org/wikipedia/en/d/d1/Five_hundred_days_of_summer.jpg",
+                    "https://www.youtube.com/watch?v=PsD0NpFSADM")
 
-goodwill_hunting.play_trailer()
+garden_state = media.Movie("Garden State",
+                    "Contains: love, for lack of a better term. Also: Natalie Portman! :D",
+                    "https://upload.wikimedia.org/wikipedia/en/3/3c/Garden_State_Poster.jpg",
+                    "https://www.youtube.com/watch?v=bCZVCU7vv78")
+
+donnie_darko = media.Movie("Donnie Darko",
+                    "'A storm is coming. A storm that will swallow the children. And I will deliver them from the kingdom of Bane. I'll deliver the children back to their doorsteps. I'll send the monsters back to the underground. I'll send them back to a place where no one else can see them except for me, because I am Donnie Darko.'",
+                    "https://upload.wikimedia.org/wikipedia/en/d/db/Donnie_Darko_poster.jpg",
+                    "https://www.youtube.com/watch?v=ZZyBaFYFySk")
+
+american_history_x = media.Movie("American History X",
+                    "I believe that you often develop a hatred for something because it so accurately defines who you are. You are convinced that you are utterly trapped in your circumstance, and you try to take pride in that mindset. And oftentimes, love is what breaks that hatred, and leads to transcendence. Here is a tragically beautiful example of this cycle of hatred and love, one that I look back to whenever I am mired in hatred.",
+                    "https://upload.wikimedia.org/wikipedia/en/0/0a/American_history_x_poster.jpg",
+                    "https://www.youtube.com/watch?v=h4GZMt8rWbU")
+
+
+# goodwill_hunting.play_trailer()
