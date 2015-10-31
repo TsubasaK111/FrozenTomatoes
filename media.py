ca32dc444878f5cdab717cb3df74da1b4e3d951d
TsubasaKondo <Tsubasa@FujiProvingGrounds.com>
2015-10-31 18:07:43 +0900

    Added draft __doc__ for media.Movie, and removed unnecessary indentation.

--- media.py
+++ media.py
@@ -1,19 +1,20 @@
 import webbrowser
 
 class Movie():
-        def __init__(self, set_title, set_storyline_text, set_poster_image, set_trailer_video):
-            self.title = set_title
-            self.storyline_text = set_storyline_text
-            self.poster_image_url = set_poster_image
-            self.trailer_video_url = set_trailer_video
-        def play_trailer(self):
-            webbrowser.open(self.trailer_video_url)
+    """This class is designed to construct movie objects that will store related information such as storyline, trailer_url, and a poster image url."""
+    def __init__(self, set_title, set_storyline_text, set_poster_image, set_trailer_video):
+        self.title = set_title
+        self.storyline_text = set_storyline_text
+        self.poster_image_url = set_poster_image
+        self.trailer_video_url = set_trailer_video
+    def play_trailer(self):
+        webbrowser.open(self.trailer_video_url)
 
 class Song():
-        def __init__(self, set_title, set_description_text, set_song_image, set_song_url):
-            self.title = set_title
-            self.description_text = set_description_text
-            self.image = set_song_image
-            self.play_url = set_song_url
-        def play_song(self):
-            webbrowser.open(self.play_url)
+    def __init__(self, set_title, set_description_text, set_song_image, set_song_url):
+        self.title = set_title
+        self.description_text = set_description_text
+        self.image = set_song_image
+        self.play_url = set_song_url
+    def play_song(self):
+        webbrowser.open(self.play_url)
